f913232b53682c6c54596e3ea19ca91be7d202fb
mhmmdfahri25 <f66208840@gmail.com>
2026-07-17 20:16:17 -0700

    Menambahkan SplashScreen

---
+++ lib/screens/splash/splash_creen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.assignment, size: 100, color: Colors.blue),
+            SizedBox(height: 20),
+            Text(
+              'Field Survey',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Aplikasi Mobile Berbasis Flutter',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
