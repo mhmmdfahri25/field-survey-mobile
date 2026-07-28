e2ed249e34d9a158e64861f2bc208030a9633b49
mhmmdfahri25 <f66208840@gmail.com>
2026-07-28 08:50:25 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/login.dart';
+import 'package:flutter_application_2/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: LoginPage()
     );
   }
 }
